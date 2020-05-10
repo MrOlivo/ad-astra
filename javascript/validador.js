33a62cf578288fa476fb7ba680afc00097e2d026
MrOlivo <46498127+MrOlivo@users.noreply.github.com>
2020-05-09 22:22:35 -0600

    Update validator.js - Change to ternary operator

--- javascript/validador.js
+++ javascript/validador.js
@@ -1,13 +1,9 @@
 
 document.getElementsByName('email')[0].addEventListener('keyup', function() {
     campo = event.target;
-    valido = document.getElementById('emailOK');
-        
+	valido = document.getElementById('emailOK');
+	
     emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-    
-    if (emailRegex.test(campo.value)) {
-      valido.innerText = "Email válido";
-    } else {
-      valido.innerText = "Email no válido";
-    }
+	
+	valido.innerText = (emailRegex.test(campo.value)) ? 'Email válido' : 'Email no válido';
 });

18a00e3e1ca35f2f0e2f8ea45f1df3145d7cd31c
MrOlivo <46498127+MrOlivo@users.noreply.github.com>
2020-10-21 16:57:32 -0600

    Update validador.js

--- javascript/validador.js
+++ javascript/validador.js
@@ -1,5 +1,5 @@
-document.getElementsByName('email')[0].addEventListener('keyup', function() {
-    const campo = event.target;
+document.getElementsByName('email')[0].addEventListener('keyup', function(e) {
+    const campo = e.target;
 	const valido = document.getElementById('emailOK');
 	
     const emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
